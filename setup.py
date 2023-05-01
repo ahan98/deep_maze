40e879f334735270ee1896a3e764e165a310c320
ahan98 <ahan98@gmail.com>
2023-05-01 04:31:14 -0400

    add dream_vae.explore

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(name='gym_maze',
       version='2.0.0',
       install_requires=['gym', 'numpy', 'pygame==2.1.2', 'scikit-image'],
-      packages=['gym_maze', 'gym_maze.envs', 'cair_maze', 'dream_vae'])
+      packages=['gym_maze', 'gym_maze.envs', 'cair_maze', 'dream_vae', 'dream_vae.explore'])
