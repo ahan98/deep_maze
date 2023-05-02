2266f50b14bfaddf72708c018bc8f1018c770d39
Alex Han <ahan98@gmail.com>
2023-05-02 17:20:30 -0400

    packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="deep-maze",
+    name="cair_maze",
     version="0.0.1",
     install_requires=["gymnasium==0.28.1", "pygame==2.1.2"],
     packages=["cair_maze", "cair_maze.envs", "dream"]
